3443a7a3f37ea21cacd39bd8de54c68051da613d
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-03-18 12:57:06 -0600

    Created Courses Microservice Module

---
+++ src/main/java/com/jaimayal/coursesmicroservice/CoursesMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.jaimayal.coursesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesMicroserviceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesMicroserviceApplication.class, args);
+    }
+}
