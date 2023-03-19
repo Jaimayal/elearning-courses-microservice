444dacb26035c2f7c3b30d33fae5fe6689d71993
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-03-19 13:06:42 -0600

    Added CourseUser relationship to save userIds inside Course Microservice

--- src/main/java/com/jaimayal/coursesmicroservice/entities/Course.java
+++ src/main/java/com/jaimayal/coursesmicroservice/entities/Course.java
@@ -1,13 +1,20 @@
 package com.jaimayal.coursesmicroservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jaimayal.coursesmicroservice.dtos.UserDTO;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,4 +29,30 @@ public class Course {
     private String description;
     private String author;
     private String category;
+    
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CourseUser> userIds;
+    
+    @Transient
+    private List<UserDTO> users;
+    
+    public Course() {
+        this.users = new ArrayList<>();
+        this.userIds = new ArrayList<>();
+    }
+    
+    public void addUserId(Long userId) {
+        CourseUser courseUser = new CourseUser();
+        courseUser.setUserId(userId);
+        userIds.add(courseUser);
+    }
+    
+    public void removeUserId(Long userId) {
+        userIds.removeIf(courseUser -> courseUser.getUserId().equals(userId));
+    }
+    
+    public boolean containsUserId(Long userId) {
+        return userIds.stream()
+                .anyMatch(courseUser -> courseUser.getUserId().equals(userId));
+    }
 }
