c131be4717a89875ef1340ad57b3b17f1d11a6a3
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-03-19 13:05:57 -0600

    Implemented FeignClient autoconfig

--- src/main/java/com/jaimayal/coursesmicroservice/CoursesMicroserviceApplication.java
+++ src/main/java/com/jaimayal/coursesmicroservice/CoursesMicroserviceApplication.java
@@ -1,9 +1,14 @@
 package com.jaimayal.coursesmicroservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class CoursesMicroserviceApplication {
     
     public static void main(String[] args) {
